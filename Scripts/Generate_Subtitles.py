8e4afc8306e26a91e5054d26963d9846856e1507
Karamveer Singh <karamveersingh2003111@Gmail.com>
2025-02-22 10:31:33 +0530

    updated

--- Scripts/Generate_Subtitles.py
+++ Scripts/Generate_Subtitles.py
@@ -3,13 +3,17 @@ import torch
 
 def transcribe_with_word_timestamps(audio_path):
     device = "cuda" if torch.cuda.is_available() else "cpu"
+    print(f"Using device: {device}")
+
     model = whisper.load_model("large").to(device)
+
+
     result = model.transcribe(audio_path, word_timestamps=True)
+
 
     word_segments = []
     for segment in result["segments"]:
         for word in segment["words"]:
             word_segments.append((word["start"], word["end"], word["word"]))
-    transcribed_text = result["text"]
 
-    return word_segments, transcribed_text
+    return word_segments
